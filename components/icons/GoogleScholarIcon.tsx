0bd77a44795ca55a46436f8875eb77392274846c
Alexandre Duarte <alexandrend@gmail.com>
2025-08-15 16:30:45 -0500

    Add files via upload

---
+++ components/icons/GoogleScholarIcon.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const GoogleScholarIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
+        <path d="M12 3L1 9l11 6 9-4.91V17h2V9L12 3zm6.82 6L12 12.72 5.18 9 12 5.28 18.82 9zM17 15.99l-5 2.73-5-2.73V12.24l5 2.73 5-2.73v3.75z" />
+    </svg>
+);
+
+export default GoogleScholarIcon;
---
+++ components/icons/LinkedInIcon.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const LinkedInIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
+        <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.25 6.5 1.75 1.75 0 016.5 8.25zM19 19h-3v-4.75c0-1.4-.5-2.25-1.5-2.25S13 12.6 13 14.25V19h-3v-9h2.9v1.35h.05c.4-.75 1.35-1.6 2.95-1.6 3.15 0 3.6 2.05 3.6 4.75V19z" />
+    </svg>
+);
+
+export default LinkedInIcon;
